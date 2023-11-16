0cdcf5911e7a4e78aa7221c140e1c1148b3b1c04
Mirian2550 <miri2550@gmail.com>
2023-11-16 01:44:26 -0300

    monedas

--- monedaydado/monedaydado.py
+++ monedaydado/monedaydado.py
@@ -1,23 +1,48 @@
 import cv2
 import numpy as np
 
-imagen = cv2.imread('monedas.jpg')
-gris = cv2.cvtColor(imagen, cv2.COLOR_BGR2GRAY)
-_, umbral = cv2.threshold(gris, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
+class DetectorObjetos:
+    def __init__(self, imagen_path):
+        self.imagen = self.redimensionar_imagen(imagen_path, 800, 600)
+        self.gris = cv2.cvtColor(self.imagen, cv2.COLOR_BGR2GRAY)
+        self.histograma_equilibrado = self.ecualizar_histograma(self.gris)
+        self.umbral = cv2.threshold(self.histograma_equilibrado, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)[1]
+        self.contornos, _ = cv2.findContours(self.umbral, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
-contornos, _ = cv2.findContours(umbral, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    def ecualizar_histograma(self, imagen):
+        equ = cv2.equalizeHist(imagen)
+        return equ
 
-for contorno in contornos:
-    area = cv2.contourArea(contorno)
-    if area > 100:  # Puedes ajustar este umbral según tu imagen
-        cv2.drawContours(imagen, [contorno], -1, (0, 255, 0), 2)
+    def redimensionar_imagen(self, imagen_path, nuevo_ancho, nuevo_alto):
+        imagen = cv2.imread(imagen_path)
+        return cv2.resize(imagen, (nuevo_ancho, nuevo_alto))
 
-cv2.imshow('Segmentación', imagen)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+    def segmentar_objetos(self, area_minima=5):
+        contornos_filtrados = [contorno for contorno in self.contornos if cv2.contourArea(contorno) > area_minima]
+        cv2.drawContours(self.imagen, contornos_filtrados, -1, (0, 255, 0), 2)
 
-# Problema 1b: Clasificación y conteo de monedas (requiere entrenamiento de modelo)
-# Aquí necesitarías un modelo previamente entrenado para clasificar las monedas.
+    def mostrar_histograma(self, imagen, titulo='Histograma'):
+        histograma = cv2.calcHist([imagen], [0], None, [256], [0, 256])
+        cv2.imshow(titulo, histograma)
+        cv2.waitKey(0)
+        cv2.destroyAllWindows()
 
-# Problema 1c: Determinación del número en cada dado (requiere entrenamiento de modelo)
-# Similar al punto anterior, necesitarías un modelo entrenado para reconocer los números en los dados.
+    def mostrar_segmentacion(self):
+        cv2.imshow('Segmentación', self.imagen)
+        cv2.waitKey(0)
+        cv2.destroyAllWindows()
+
+# Crear un objeto DetectorObjetos
+detector = DetectorObjetos('monedas.jpg')
+
+# Mostrar el histograma antes de la segmentación
+detector.mostrar_histograma(detector.gris, 'Histograma Antes')
+
+# Segmentar monedas con un área mínima de 5
+detector.segmentar_objetos()
+
+# Mostrar el histograma después de la segmentación
+detector.mostrar_histograma(detector.umbral, 'Histograma Después')
+
+# Mostrar la segmentación
+detector.mostrar_segmentacion()

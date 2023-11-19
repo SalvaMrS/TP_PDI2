fd7c36dc15e3d783991ecd6bf6f9940dff516a89
pepeargentoo <dani_rc_22@hotmail.com>
2023-11-19 19:12:15 -0300

    se agrego segmentacion

--- clasificador_patentes/clasificador.py
+++ clasificador_patentes/clasificador.py
@@ -4,10 +4,11 @@ import numpy as np
 import cv2
 import tensorflow as tf
 
-
 physical_devices = tf.config.experimental.list_physical_devices('GPU')
 if len(physical_devices) > 0:
     tf.config.experimental.set_memory_growth(physical_devices[0], True)
+
+
 class DetectorPatente:
     MODEL_PATH = 'clasificador_patentes/model/tf-yolo_tiny_v4-512x512-custom-anchors'
     INPUT_SIZE = 512
@@ -18,7 +19,7 @@ class DetectorPatente:
         self.saved_model_loaded = tf.saved_model.load(self.MODEL_PATH, tags=[tag_constants.SERVING])
         self.yolo_infer = self.saved_model_loaded.signatures['serving_default']
 
-    """def preprocess(self, frame: np.ndarray) -> tf.Tensor:
+    def preprocess(self, frame: np.ndarray) -> np.ndarray:
         # Convertir la imagen a escala de grises
         gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
@@ -32,63 +33,83 @@ class DetectorPatente:
         # Dilatación adicional para resaltar los contornos
         morphological = cv2.dilate(morphological, kernel, iterations=1)
 
-        # Encuentra los contornos en la imagen
+        # Encuentra los contornos en la imagen original para dibujarlos
         contours, _ = cv2.findContours(morphological, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
-        # Si se encontraron contornos, dibuja un rectángulo alrededor del área del contorno más grande (asumiendo que es la patente)
-        if contours:
-            max_contour = max(contours, key=cv2.contourArea)
-            x, y, w, h = cv2.boundingRect(max_contour)
-            cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+        # Dibujar contornos en la imagen original
+        cv2.drawContours(frame, contours, -1, (0, 255, 0), 2)
 
-        # Redimensionar la imagen a las dimensiones de entrada del modelo
-        resized_frame = cv2.resize(frame, (512, 512))
-
-        # Mostrar la imagen con el rectángulo dibujado
-        cv2.imshow('Imagen con rectángulo de patente', resized_frame)
+        # Mostrar la imagen con contornos dibujados
+        cv2.imshow('Imagen con Contornos', frame)
         cv2.waitKey(0)
         cv2.destroyAllWindows()
 
-        # Convertir a tf.Tensor y asegurarse de que sea float32
-        return tf.constant(resized_frame, dtype=tf.float32)"""
+        return morphological
 
-    def preprocess(self,image):
-        gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-        _, binary = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
-        morph = cv2.morphologyEx(binary, cv2.MORPH_CLOSE, np.ones((5, 5), np.uint8))
-        return morph
-
-    def detect_and_segment(self,image_path):
-        image = cv2.imread(image_path)
-        processed_image = self.preprocess(image)
-
-        contours, _ = cv2.findContours(processed_image, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    def postprocess_contours(self, image, contours):
+        # Inicializar lista para almacenar coordenadas de la patente
+        patente_coords = []
 
         for contour in contours:
             area = cv2.contourArea(contour)
-            if area > 1000:  # Filtro de área para eliminar contornos pequeños
-                x, y, w, h = cv2.boundingRect(contour)
-                cv2.rectangle(image, (x, y), (x + w, y + h), (0, 255, 0), 2)
-                roi = image[y:y + h, x:x + w]
-                cv2.imshow('Segmento de Patente', roi)
+            epsilon = 0.02 * cv2.arcLength(contour, True)
+            approx = cv2.approxPolyDP(contour, epsilon, True)
 
-        cv2.imshow('Contornos Detectados', image)
+            if len(approx) >= 3 and area > 1000 and area < 5000:  # Filtro de área y forma
+                rect = cv2.minAreaRect(contour)
+                box = cv2.boxPoints(rect)
+                box = np.int0(box)
+                cv2.drawContours(image, [box], 0, (0, 255, 0), 2)
+                patente_coords.append(box)
+
+        return image, patente_coords
+
+    def apply_additional_filters(self, image):
+        # Aplicar más filtros y operaciones morfológicas según sea necesario
+        gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+        _, thresholded = cv2.threshold(gray, 128, 255, cv2.THRESH_BINARY)
+
+        # Aplicar operaciones morfológicas adicionales
+        kernel = np.ones((5, 5), np.uint8)
+        morphological = cv2.morphologyEx(thresholded, cv2.MORPH_CLOSE, kernel)
+        morphological = cv2.dilate(morphological, kernel, iterations=1)
+
+        # Puedes experimentar con más filtros y operaciones aquí
+
+        return morphological
+
+    def perspective_transform(self, image, box):
+        rect = np.array(box, dtype="float32")
+        width, height = 512, 512  # Ajusta las dimensiones según sea necesario
+        dst = np.array([[0, 0], [width - 1, 0], [width - 1, height - 1], [0, height - 1]], dtype="float32")
+        M = cv2.getPerspectiveTransform(rect, dst)
+        warped = cv2.warpPerspective(image, M, (width, height))
+        return warped
+
+    def detect_and_segment(self, image_path):
+        image = cv2.imread(image_path)
+        processed_image = self.preprocess(image.copy())
+
+        # Detección de contornos
+        contours, _ = cv2.findContours(processed_image, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+        # Postprocesamiento de contornos aplicando más filtros y operaciones morfológicas
+        result_image, patente_coords = self.postprocess_contours(image.copy(), contours)
+
+        # Dibuja solo la patente en la imagen original
+        for box in patente_coords:
+            roi = self.perspective_transform(image.copy(), box)
+            cv2.imshow('Segmento de Patente', roi)
+
+        # Aplicar filtros adicionales y operaciones morfológicas
+        additional_processed_image = self.apply_additional_filters(image.copy())
+
+        cv2.imshow('Contornos Detectados (Postprocesamiento)', result_image)
+        cv2.imshow('Imagen con Filtros Adicionales', additional_processed_image)
         cv2.waitKey(0)
         cv2.destroyAllWindows()
-    def correct_perspective(self, frame: np.ndarray) -> np.ndarray:
-        # Implementa tu lógica de corrección de perspectiva aquí
-        # Puedes usar cv2.warpPerspective para esto
-        # Ejemplo simple:
-        pts1 = np.float32([[56,65],[368,52],[28,387],[389,390]])
-        pts2 = np.float32([[0,0],[300,0],[0,300],[300,300]])
-        matrix = cv2.getPerspectiveTransform(pts1,pts2)
-        corrected_frame = cv2.warpPerspective(frame, matrix, (300,300))
-        return corrected_frame
 
     def adaptive_threshold(self, image: np.ndarray) -> np.ndarray:
-        # Implementa tu lógica de umbralización adaptativa aquí
-        # Puedes usar cv2.adaptiveThreshold para esto
-        # Ejemplo simple:
         image_gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
         _, thresh = cv2.threshold(image_gray, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
         return thresh
@@ -126,24 +147,6 @@ class DetectorPatente:
         input_img = self.preprocess(frame)
         print("Input shape:", input_img.shape)
         yolo_out = self.predict(input_img)
-        """
-        bboxes = self.procesar_salida_yolo(yolo_out)
-        # Convertir el frame completo a escala de grises una vez
-
-        iter_coords = self.yield_coords(frame, bboxes)
-
-        for x1, y1, x2, y2, _ in iter_coords:
-            cv2.rectangle(frame, (x1, y1), (x2, y2), (36, 255, 12), 2)
-            #gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-            plate, probs = PatenteOCR.predict_ocr(x1, y1, x2, y2, frame)
-            # print(plate)
-            avg = np.mean(probs)
-
-            if avg > self.ocr.confianza_avg:
-                plate = ''.join(plate).replace('_', '')
-                print(plate)
-                return plate
-        """
 
     @staticmethod
     def yield_coords(frame: np.ndarray, bboxes: list):

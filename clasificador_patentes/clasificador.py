ea11abcbae06f9f2250769bfe2ccffce0a2e8883
pepeargentoo <dani_rc_22@hotmail.com>
2023-11-19 11:51:31 -0300

    avances patentes

--- clasificador_patentes/clasificador.py
+++ clasificador_patentes/clasificador.py
@@ -1,130 +1,137 @@
-import cv2
+from tensorflow.python.saved_model import tag_constants
 import numpy as np
-import os
-from skimage import measure
-from skimage.filters import threshold_local
-from skimage import segmentation
-import matplotlib.pyplot as plt
+import cv2
+import tensorflow as tf
 
-# Cargar el modelo YOLO preentrenado y configuración
-net = cv2.dnn.readNet("clasificador_patentes/model/yolov3.weights", "clasificador_patentes/model/yolov3.cfg")
-layer_names = net.getUnconnectedOutLayersNames()
+class DetectorPatente:
+    """
+    Module responsible for license plate detection.
+    """
 
-# Función para procesar una imagen y detectar patentes
-def detect_and_segment_license_plate(image_path):
-    # Cargar la imagen
-    image = cv2.imread(image_path)
-    height, width, _ = image.shape
+    MODEL_PATH = 'clasificador_patentes/model/tf-yolo_tiny_v4-512x512-custom-anchors'
+    INPUT_SIZE = 512
+    IOU_THRESHOLD = 0.45
+    SCORE_THRESHOLD = 0.25
 
-    # Preprocesamiento de la imagen
-    blob = cv2.dnn.blobFromImage(image, 0.00392, (416, 416), (0, 0, 0), True, crop=False)
-    net.setInput(blob)
-    outs = net.forward(layer_names)
+    def __init__(self):
+        """
+        Initialize the PlateDetector with the fixed YOLO model.
+        """
+        self.saved_model_loaded = tf.saved_model.load(self.MODEL_PATH, tags=[tag_constants.SERVING])
+        self.yolo_infer = self.saved_model_loaded.signatures['serving_default']
 
-    # Postprocesamiento para obtener las cajas delimitadoras
-    class_ids = []
-    confidences = []
-    boxes = []
+    def preprocess(self, frame: np.ndarray) -> tf.Tensor:
+        """
+        Mejora el preprocesamiento de la imagen.
 
-    for out in outs:
-        for detection in out:
-            scores = detection[5:]
-            class_id = np.argmax(scores)
-            confidence = scores[class_id]
+        Parameters:
+            frame (np.ndarray): Imagen de entrada.
 
-            if confidence > 0.5 and class_id == 2:  # Clase 2 representa patentes en el modelo YOLO
-                center_x, center_y, w, h = (detection[:4] * np.array([width, height, width, height])).astype(int)
-                x, y = int(center_x - w / 2), int(center_y - h / 2)
-                boxes.append([x, y, w, h])
-                confidences.append(float(confidence))
-                class_ids.append(class_id)
+        Returns:
+            tf.Tensor: Tensor de imagen preprocesado.
+        """
+        # Asegúrate de que la imagen tenga el rango correcto (0-1)
+        image_data = frame.astype(np.float32) / 255.0
 
-    # Eliminar cajas solapadas
-    indices = cv2.dnn.NMSBoxes(boxes, confidences, 0.5, 0.4)
-    indices = list(indices)
+        # Aumento de datos aleatorio: ajuste de brillo y contraste
+        alpha = 0.5 + np.random.uniform(-0.2, 0.2)  # factor de ajuste de contraste
+        beta = 0.5 + np.random.uniform(-0.2, 0.2)   # factor de ajuste de brillo
+        image_data = cv2.convertScaleAbs(image_data, alpha=alpha, beta=beta)
 
-    # Mostrar resultados de detección
-    for i in indices:
-        try:
-            x, y, w, h = boxes[i]
+        # Aumento de datos aleatorio: rotación
+        angle = np.random.uniform(-10, 10)  # ángulo de rotación en grados
+        rows, cols, _ = image_data.shape
+        rotation_matrix = cv2.getRotationMatrix2D((cols / 2, rows / 2), angle, 1)
+        image_data = cv2.warpAffine(image_data, rotation_matrix, (cols, rows))
 
-            # Ajustar coordenadas negativas
-            x, y, w, h = map(max, (x, y, w, h), (0, 0, 0, 0))
+        # Resize de la imagen a las dimensiones de entrada del modelo
+        image_data = cv2.resize(image_data, (512, 512))
 
-            # Verificar que las dimensiones de la región de la patente sean válidas
-            if 0 < w <= width and 0 < h <= height:
-                plate_image = image[y:y + h, x:x + w]
+        # Normaliza la imagen restando la media y dividiendo por la desviación estándar
+        mean = [0.485, 0.456, 0.406]  # Media de ImageNet
+        std = [0.229, 0.224, 0.225]   # Desviación estándar de ImageNet
+        image_data = (image_data - mean) / std
 
-                # Mostrar resultados
-                cv2.rectangle(image, (x, y), (x + w, y + h), (0, 255, 0), 2)
-                cv2.imshow('Segmented License Plate', plate_image)
-                cv2.waitKey(0)
-                cv2.destroyAllWindows()
-            else:
-                print("La región de la patente no tiene dimensiones válidas.")
-        except Exception as e:
-            print(f"Error al procesar la imagen {image_path}: {str(e)}")
-            print(f"Index en la lista de índices: {i}")
-            continue
+        # Añade una dimensión para representar el batch (1 imagen)
+        image_data = np.expand_dims(image_data, axis=0)
 
+        # Convierte a tf.Tensor
+        return tf.constant(image_data)
 
-def segment_characters(license_plate_image):
-    # Convertir la imagen a escala de grises
-    gray = cv2.cvtColor(license_plate_image, cv2.COLOR_BGR2GRAY)
+    def predict(self, input_img: tf.Tensor) -> dict:
+        """
+        Make an inference based on the input tensor.
 
-    # Aplicar umbral local para obtener una imagen binaria
-    thresh = threshold_local(gray, 21, offset=5, method="gaussian")
-    binary = (gray > thresh).astype("uint8") * 255
+        Parameters:
+            input_img (tf.Tensor): Input image tensor.
 
-    # Encontrar contornos en la imagen binaria
-    contours, _ = cv2.findContours(binary, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+        Returns:
+            dict: Output of the YOLO model.
+        """
+        return self.yolo_infer(input_img)
 
-    # Filtrar contornos por área para eliminar pequeños objetos no deseados
-    min_area = 100
-    contours = [cnt for cnt in contours if cv2.contourArea(cnt) > min_area]
+    def procesar_salida_yolo(self, output: dict) -> list:
+        """
+        Process the output of YOLO.
 
-    # Crear una máscara en blanco
-    mask = np.zeros_like(binary)
+        Parameters:
+            output (dict): Output dictionary from YOLO.
 
-    # Dibujar los contornos en la máscara
-    cv2.drawContours(mask, contours, -1, (255), thickness=cv2.FILLED)
+        Returns:
+            list: Bounding Boxes of all detected license plates after NMS.
+        """
+        for key, value in output.items():
+            boxes = value[:, :, 0:4]
+            pred_conf = value[:, :, 4:]
 
-    # Segmentar los caracteres usando la máscara
-    segmented_characters = cv2.bitwise_and(binary, mask)
+        boxes, scores, classes, valid_detections = tf.image.combined_non_max_suppression(
+            boxes=tf.reshape(boxes, (tf.shape(boxes)[0], -1, 1, 4)),
+            scores=tf.reshape(
+                pred_conf, (tf.shape(pred_conf)[0], -1, tf.shape(pred_conf)[-1])),
+            max_output_size_per_class=50,
+            max_total_size=50,
+            iou_threshold=self.IOU_THRESHOLD,
+            score_threshold=self.SCORE_THRESHOLD
+        )
+        return [boxes.numpy(), scores.numpy(), classes.numpy(), valid_detections.numpy()]
 
-    return segmented_characters, contours
+    def draw_bboxes(self, frame: np.ndarray, bboxes: list, mostrar_score: bool = False) -> np.ndarray:
+        """
+        Draw bounding boxes on the frame.
 
+        Parameters:
+            frame (np.ndarray): Original frame.
+            bboxes (list): Predictions after NMS.
+            mostrar_score (bool): Whether to display scores.
 
-# Carpeta que contiene las imágenes
-carpeta_imagenes = 'Patentes/Patentes/'
-archivos_en_carpeta = os.listdir(carpeta_imagenes)
-extensiones_validas = ['.png']
-archivos_imagen = [archivo for archivo in archivos_en_carpeta if any(archivo.endswith(ext) for ext in extensiones_validas)]
+        Returns:
+            np.ndarray: Frame with drawn bounding boxes.
+        """
+        for x1, y1, x2, y2, score in self.yield_coords(frame, bboxes):
+            font_scale = 2
+            cv2.rectangle(frame, (x1, y1), (x2, y2), (36, 255, 12), 2)
+            cv2.putText(frame, f'{score:.2f}%', (x1, y1 - 40), cv2.FONT_HERSHEY_SIMPLEX,
+                        font_scale, (20, 10, 220), 5)
+        return frame
 
-print(archivos_imagen)
+    @staticmethod
+    def yield_coords(frame: np.ndarray, bboxes: list):
+        """
+        Yield coordinates of the rectangles.
 
+        Parameters:
+            frame (np.ndarray): Original frame.
+            bboxes (list): Predictions after NMS.
 
-
-
-
-# Procesar cada imagen
-for imagen in archivos_imagen:
-
-    imagen_path = os.path.join(carpeta_imagenes, imagen)
-    license_plate_image = cv2.imread(imagen_path)
-
-    # Segmentar caracteres
-    segmented_characters, contours = segment_characters(license_plate_image)
-    plt.figure(figsize=(12, 4))
-
-    plt.subplot(1, 3, 1), plt.imshow(cv2.cvtColor(license_plate_image, cv2.COLOR_BGR2RGB)), plt.title('Imagen Original')
-    plt.subplot(1, 3, 2), plt.imshow(segmented_characters, cmap='gray'), plt.title('Caracteres Segmentados')
-    plt.subplot(1, 3, 3), plt.imshow(cv2.cvtColor(license_plate_image, cv2.COLOR_BGR2RGB)), plt.title('Contornos')
-
-    # Dibujar contornos en la imagen original
-    contour_image = license_plate_image.copy()
-    cv2.drawContours(contour_image, contours, -1, (0, 255, 0), 2)
-    plt.subplot(1, 3, 3), plt.imshow(cv2.cvtColor(contour_image, cv2.COLOR_BGR2RGB)), plt.title('Contornos')
-
-    plt.show()
-    #detect_and_segment_license_plate(imagen_path)
+        Yields:
+            tuple: Coordinates and score.
+        """
+        out_boxes, out_scores, out_classes, num_boxes = bboxes
+        image_h, image_w, _ = frame.shape
+        for i in range(num_boxes[0]):
+            coor = out_boxes[0][i]
+            x1 = int(coor[1] * image_w)
+            y1 = int(coor[0] * image_h)
+            x2 = int(coor[3] * image_w)
+            y2 = int(coor[2] * image_h)
+            yield x1, y1, x2, y2, out_scores[0][i]
--- clasificador_patentes/main.py
+++ clasificador_patentes/main.py
@@ -1,17 +1,48 @@
-import  os
+import os
 import cv2
-from clasificador_patentes.ocr import PatenteOCR
-ocr_detector = PatenteOCR()
 
-# Ruta de la imagen que deseas procesar
+from clasificador_patentes.clasificador import DetectorPatente
+from clasificador_patentes.ocr import PatenteOCR
+"""ocr_detector = PatenteOCR()
+
 
 carpeta_imagenes = 'Patentes/Patentes/'
 archivos_en_carpeta = os.listdir(carpeta_imagenes)
-extensiones_validas = ['.png','.jpeg', '.jpeg','.webp']
-archivos_imagen = [archivo for archivo in archivos_en_carpeta if any(archivo.endswith(ext) for ext in extensiones_validas)]
+extensiones_validas = ['.png', '.jpg', '.jpeg']
+archivos_imagen = [archivo for archivo in archivos_en_carpeta if any(archivo.lower().endswith(ext) for ext in extensiones_validas)]
+
 for imagen in archivos_imagen:
+    plate_detector = DetectorPatente()
+    imagen_path = os.path.join(carpeta_imagenes, imagen)
+    image = cv2.imread(imagen_path)
+
+    if image is None:
+        print(f"Error: Unable to read image {imagen_path}")
+        continue
+
+    processed_image = plate_detector.preprocess(image)
+    detections = plate_detector.predict(processed_image)
+    bounding_boxes = plate_detector.procesar_salida_yolo(detections)
+
+    if bounding_boxes:
+        result_image = plate_detector.draw_bboxes(image, bounding_boxes)
+        cv2.imshow(f'Detected Plates - {imagen}', result_image)
+    else:
+        print(f"No license plate detected in the image: {imagen}")
+
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+"""
+carpeta_imagenes = 'Patentes/Patentes/'
+archivos_en_carpeta = os.listdir(carpeta_imagenes)
+extensiones_validas = ['.png', '.jpg', '.jpeg']
+ocr_detector = PatenteOCR()
+archivos_imagen = [archivo for archivo in archivos_en_carpeta if any(archivo.lower().endswith(ext) for ext in extensiones_validas)]
+for imagen in archivos_imagen:
+    plate_detector = DetectorPatente()
     imagen_path = os.path.join(carpeta_imagenes, imagen)
     frame = cv2.imread(imagen_path)
+
     # Obtener las dimensiones de la imagen
     height, width, _ = frame.shape
     # Definir coordenadas que cubran toda la imagen
@@ -22,4 +53,8 @@ for imagen in archivos_imagen:
     print(patentes_detectadas, imagen_path)
     # Imprimir los valores de las patentes detectadas
     for i, patente in enumerate(patentes_detectadas, 1):
+        cv2.imshow(patente, frame)
+        cv2.waitKey(0)
+        cv2.destroyAllWindows()
         print(f"Patente {i}: {patente}")
+

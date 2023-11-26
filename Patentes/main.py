71da706fac9355e5125c862144ef04a9f912a6d0
pepeargentoo <dani_rc_22@hotmail.com>
2023-11-26 17:00:20 -0300

    mmmm

--- Patentes/main.py
+++ Patentes/main.py
@@ -3,7 +3,6 @@ import os
 import matplotlib.pyplot as plt
 import numpy as np
 from Funciones import *
-from Patentes.segmentation import SegmentationProcessor
 
 def imshow(img, new_fig=True, title=None, color_img=False, blocking=False, colorbar=True, ticks=False):
     if new_fig:
@@ -52,28 +51,46 @@ tuplas_imagenes = list(zip(imagenes, imagenes_sobel_umbralizado2))
 subimagenes_zona_seleccionada = transformar_imagenes(tuplas_imagenes, obtener_subimagen_zona_seleccionada)
 for subimg in subimagenes_zona_seleccionada:
     subimg_gray = cv2.cvtColor(subimg, cv2.COLOR_BGR2GRAY)
-    _, subimg_humbr = cv2.threshold(subimg_gray, 72,255, cv2.THRESH_BINARY)
+    _, subimg_humbr = cv2.threshold(subimg_gray, 90, 255, cv2.THRESH_OTSU)
+    threshol = _ +(_*0.12)
+    _, subimg_humbr = cv2.threshold(subimg_gray, _ + 20 , 255, cv2.THRESH_BINARY)
 
-    se = cv2.getStructuringElement(cv2.MORPH_RECT , (5,5))
-    # binary_img = cv2.morphologyEx(subimg_humbr, cv2.MORPH_OPEN, se)
 
-    binary_img = cv2.morphologyEx(subimg_humbr, cv2.MORPH_GRADIENT, se)
     retval, labels, stats, centroids = cv2.connectedComponentsWithStats(subimg_humbr)
-
 
     # Imprime la cantidad de componentes encontrados
     print("Número total de componentes conectados:", retval)
-    cv2.imshow('lll', subimg)
+
+    cv2.imshow('Imagen Original', subimg)
+
     # Imprime las estadísticas de cada componente
     for i in range(1, retval):  # Comienza desde 1 para omitir el componente de fondo (etiqueta 0)
-        obj = (labels == i).astype(np.uint8)*255
-        contours, _ = cv2.findContours(obj, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
-        obj_color = cv2.merge((obj,obj,obj))
-        cv2.drawContours(obj_color, contours, -1, (255,0,0), 1)
-        st = stats[i,:]
-        cv2.rectangle(obj_color, (st[0],st[1]),(st[0]+st[2], st[1]+st[3]), color=(0,0,255), thickness=1)
-        cv2.imshow('oo', obj_color)
-        cv2.waitKey(0)
+        obj = (labels == i).astype(np.uint8) * 255
+        contours, _ = cv2.findContours(obj, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+        # Filtrar por área mínima
+        area = cv2.contourArea(contours[0])
+        if area < 5 or area > 340:
+            continue
+
+        # Aproximación de forma
+        epsilon = 0.04 * cv2.arcLength(contours[0], True)
+        approx = cv2.approxPolyDP(contours[0], epsilon, True)
+
+        # Filtrar por forma (por ejemplo, número de lados)
+        if len(approx) > 3:  # Asumiendo que estás buscando letras y números
+
+            obj_color = cv2.merge((obj, obj, obj))
+            cv2.drawContours(obj_color, [approx], -1, (255, 0, 0), 1)
+
+            st = stats[i, :]
+            cv2.rectangle(obj_color, (st[0], st[1]), (st[0] + st[2], st[1] + st[3]), color=(0, 0, 255), thickness=1)
+
+            cv2.imshow('Objeto Filtrado', obj_color)
+            cv2.waitKey(0)
+
+    # Espera la entrada del teclado antes de pasar a la siguiente imagen
+    cv2.waitKey(0)
 cv2.destroyAllWindows()
 
 
